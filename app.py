33c76ba97ce3f68495e74f1a2721a1507892d8fd
zeeshan shah <zpert.2000@hotmail.uk>
2025-02-18 14:48:38 +0000

    Updated model with new hyperparameterss

--- app.py
+++ app.py
@@ -1,3 +1,2 @@
-git config --global user.name "zeeshan shah "
-git config --global user.email "zpert.2000@hotmail.uk"
-git remote add origin https://github.com/yourusername/ml_project.git
+git config --global user.name " "
+git config --global user.email ""
